ccaa76cc28845e71e853387ce9e02ad9f73a1d79
shaimaa473 <75483443+shaimaa473@users.noreply.github.com>
2021-08-25 18:23:07 +0200

    Update TypeS.ts

--- TypeS.ts
+++ TypeS.ts
@@ -16,3 +16,9 @@ function add(x: number, y: number, z?: number) {
 
 console.log(add(3,5)); // expected output 8
 console.log(add(2, 3, 4)); // expected output 9
+
+// Question (2)
+// Loose Coupling in class : means one class is dependent on interface rather than class.
+// All systems can work independently, as part of the larger group of systems.
+// Nothing is forced into a relationship with anything else. 
+// This make the program more flexable.
